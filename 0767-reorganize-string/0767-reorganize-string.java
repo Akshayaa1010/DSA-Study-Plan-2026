86701e3b8a7976d1d68e3659f8cf2cf501f671ef
Akshayaa M <akshayaamaharishi@gmail.com>
2026-09-24 13:26:07 +0530

    Time: 0 ms (100%), Space: 43 MB (71.07%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -0,0 +1,43 @@
+class Solution {
+        public String reorganizeString(String s) {
+                int[] freq = new int[26];
+                        for (char c : s.toCharArray()) {
+                                    freq[c - 'a']++;
+                                            }
+
+                                                    int maxFreq = 0;
+                                                            int maxChar = 0;
+                                                                    for (int i = 0; i < 26; i++) {
+                                                                                if (freq[i] > maxFreq) {
+                                                                                                maxFreq = freq[i];
+                                                                                                                maxChar = i;
+                                                                                                                            }
+                                                                                                                                    }
+
+                                                                                                                                            if (maxFreq > (s.length() + 1) / 2) {
+                                                                                                                                                        return "";
+                                                                                                                                                                }
+
+                                                                                                                                                                        char[] res = new char[s.length()];
+                                                                                                                                                                                int idx = 0;
+
+                                                                                                                                                                                        while (freq[maxChar] > 0) {
+                                                                                                                                                                                                    res[idx] = (char) (maxChar + 'a');
+                                                                                                                                                                                                                idx += 2;
+                                                                                                                                                                                                                            freq[maxChar]--;
+                                                                                                                                                                                                                                    }
+
+                                                                                                                                                                                                                                            for (int i = 0; i < 26; i++) {
+                                                                                                                                                                                                                                                        while (freq[i] > 0) {
+                                                                                                                                                                                                                                                                        if (idx >= res.length) {
+                                                                                                                                                                                                                                                                                            idx = 1;
+                                                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                                                                                            res[idx] = (char) (i + 'a');
+                                                                                                                                                                                                                                                                                                                                            idx += 2;
+                                                                                                                                                                                                                                                                                                                                                            freq[i]--;
+                                                                                                                                                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                                                                                                                                                                }
+
+                                                                                                                                                                                                                                                                                                                                                                                        return new String(res);
+                                                                                                                                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                                                                                                                                                            }
